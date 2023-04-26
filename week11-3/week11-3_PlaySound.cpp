745a2398614af947cd0e7017c25f5cd473046012
marinesea99 <abc0972521139@gmail.com>
2023-04-26 12:04:55 +0800

    week11

---
+++ week11-3/week11-3_PlaySound.cpp
@@ -0,0 +1,8 @@
+#include <windows.h>
+int main()
+{                       ///要等待同步    SND_ASYNC
+    PlaySound("do_re_mi\\do.wav",   NULL,SND_SYNC);
+    PlaySound("do_re_mi\\re.wav",   NULL,SND_SYNC);
+    PlaySound("do_re_mi\\mi.wav",   NULL,SND_SYNC);
+    ///PlaySound("C:\\Users\\Administrator\\Desktop\\do_re_mi\\mi.wav",   NULL,SND_SYNC);
+}
